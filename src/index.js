aa119ce3b1ef7eb5b6496bc36e9c34717bf47ec7
Marc Thomas <hello@mrcthms.com>
2017-10-22 11:43:01 +0100

    Add more packages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import printMe from './print';
-
+import { cube } from './math';
 function component() {
   const element = document.createElement('div');
   let btn = document.createElement('button');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = ['Hello', 'webpack'].join(' ');
   btn.innerHTML = 'Click me and check the console! And then what?!';
   btn.onclick = printMe;
   element.appendChild(btn);
+
+  const pre = document.createElement('pre');
+  pre.innerHTML = ['Hello Webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n');
+  element.appendChild(pre);
   return element;
 }
 
